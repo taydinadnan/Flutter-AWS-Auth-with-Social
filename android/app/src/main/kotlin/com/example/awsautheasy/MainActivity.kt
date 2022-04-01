d9a7c1e7282307d02ca011f6cd164030eb5f5212
Turgay Aydin <taydinadnan@gmail.com>
2022-04-02 00:05:47 +0200

    authentication Email, Google , Facebook

---
+++ android/app/src/main/kotlin/com/example/awsautheasy/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.awsautheasy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_authenticator/amplify_authenticator.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
+
+import 'amplifyconfiguration.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    _configureAmplify();
+  }
+
+  Future<void> _configureAmplify() async {
+    try {
+      await Amplify.addPlugin(AmplifyAuthCognito());
+      await Amplify.configure(amplifyconfig);
+    } on Exception catch (e) {
+      print('Could not configure Amplify: $e');
+    }
+  }
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Authenticator(
+      child: MaterialApp(
+        theme: ThemeData.light(),
+        darkTheme: ThemeData.dark(),
+
+        // `builder` must be specified with `Authenticator.builder()`.
+        builder: Authenticator.builder(),
+
+        // You can use any combination of `routes`, `home`,
+        // `onGenerateRoute`, etc. as long as `builder` is
+        // configured as shown above.
+        home: const LoggedInScreen(),
+      ),
+    );
+  }
+}
+
+class LoggedInScreen extends StatelessWidget {
+  const LoggedInScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Text('Logged In'),
+            SignOutButton(),
+          ],
+        ),
+      ),
+    );
+  }
+}
